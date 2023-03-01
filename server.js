2066423a03386b862d9084c6435c48a0eda81c75
Vincent Avez <vincent.avez22@gmail.com>
2023-03-01 23:38:02 +0100

    Modification variable environment

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 import express from 'express'
 import ejs from 'ejs'
 import path from 'path'
-import https from 'https'
-import fs from 'node:fs'
 
 import bodyParser from 'body-parser'
 import session from 'express-session'
-import dotenv from 'dotenv'
+
 import { isAuthentification } from './src/services/session.js'
 
 
 import route from './src/routes/routes.js'
 import { createDB } from './src/db/createDB.js'
 
-dotenv.config()
-
 const app = express()
-const port = process.env.PORT || 443
-dotenv.config()
+const port = 4000
 
-const options = {
-    key: fs.readFileSync('path/to/private.key'),
-    cert: fs.readFileSync('path/to/certificate.crt')
-  };
-https.createServer(options, app)
+
+
 
 app.engine('.html', ejs.__express)
 app.set('views', path.join(process.cwd(), 'views'));
@@ -33,10 +25,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(express.static('uploads'))
 app.use(isAuthentification)
-app.use((req, res, next) => {
-    res.locals.loader = true;
-    next();
-});
 app.use(session({
     secret: process.env.SECRET_SESSION,
     resave: false,
